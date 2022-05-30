e4fac0a72ce5d299a75caa1ba2202b92c815db7a
Bernadett Varga <detty.v@gmail.com>
2022-05-30 09:56:32 +0200

    feat: add deploy command to gh pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,11 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+  // For github pages
+  router: {
+    base: '/portfolio-site/'
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'portfolio-site',
