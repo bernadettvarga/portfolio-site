aef7f133a7c8f7702fb6f0dfb1968da1d199c544
Bernadett Varga <detty.v@gmail.com>
2022-05-31 15:31:40 +0200

    feat: add customizable title and description for gallery component

--- components/Gallery.vue
+++ components/Gallery.vue
@@ -1,10 +1,15 @@
 <template>
   <div class="p-6 md:pt-0 flex flex-1 items-stretch space-x-4">
-    <div class="w-72 self-end flex-shrink-0">
-      <p class="font-display leading-5 text-lg p-3 pr-10">
-        Bernadett Varga is a self-thaught photographer whose main focus is fashion, portrait and product photography. She has been taking pictures for 12 years now and since then worked with local designers.
-      </p>
-    </div>
+    <template v-if="title || description">
+      <div class="w-72 self-end flex-shrink-0">
+        <template v-if="title">
+          <h1 class="text-5xl">{{ title }}</h1>
+        </template>
+        <template v-if="description">
+          <p class="font-display leading-5 text-lg p-10 pl-0 pt-5">{{ description }}</p>
+        </template>
+      </div>
+    </template>
     <div class="flex-shrink-0">
       <img src="~/assets/img/overview/te-3.jpg" class="gallery-image__portrait" />
     </div>
@@ -23,5 +28,15 @@
 <script>
 export default {
   name: 'Gallery',
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+    description: {
+      type: String,
+      default: '',
+    },
+  },
 }
 </script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <Gallery />
+  <Gallery :description="description" />
 </template>
 
 <script>
@@ -8,6 +8,11 @@ import Gallery from '@/components/Gallery'
 export default {
   components: {
     Gallery,
-  }
+  },
+  data() {
+    return {
+      description: ' Bernadett Varga is a self-thaught photographer based in Budapest whose main focus is fashion, portrait and product photography. She has been taking pictures for 12 years now and since then worked with local designers.',
+    }
+  },
 }
 </script>
