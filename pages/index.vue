71fb1f262dce89fd41f94183d950b236a058bfc0
Bernadett Varga <detty.v@gmail.com>
2022-05-31 20:37:42 +0200

    refact: outsource content object to separate file

--- pages/index.vue
+++ pages/index.vue
@@ -4,6 +4,7 @@
 
 <script>
 import Gallery from '@/components/Gallery'
+import contentObj from '@/components/content/gallery/overview.js'
 
 export default {
   components: {
@@ -13,27 +14,7 @@ export default {
     return {
       description:
         'Bernadett Varga is a self-thaught photographer based in Budapest whose main focus is fashion, portrait and product photography. She has been taking pictures for 12 years now and since then worked with local designers.',
-      content: {
-        folder: 'overview',
-        items: [
-          {
-            src: 'te-3',
-            type: 'portrait',
-          },
-          {
-            src: 'mad-prod-2',
-            type: 'portrait',
-          },
-          {
-            src: 'enroute-french-2-2',
-            type: 'portrait',
-          },
-          {
-            src: 'sb-1',
-            type: 'landscape',
-          },
-        ],
-      },
+      content: contentObj,
     }
   },
 }
