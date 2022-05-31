e98d86302f5a815083f1c0e971b4ed98c1b9221f
Bernadett Varga <detty.v@gmail.com>
2022-05-31 11:46:43 +0200

    feat: add intro

--- components/Gallery.vue
+++ components/Gallery.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="p-6 flex flex-1 items-stretch space-x-4">
+  <div class="p-6 md:pt-0 flex flex-1 items-stretch space-x-4">
+    <div class="w-72 self-end flex-shrink-0">
+      <p class="font-display leading-5 text-lg p-3 pr-10">
+        Bernadett Varga is a self-thaught photographer whose main focus is fashion, portrait and product photography. She has been taking pictures for 12 years now and since then worked with local designers.
+      </p>
+    </div>
     <div class="flex-shrink-0">
       <img src="~/assets/img/overview/te-3.jpg" class="gallery-image__portrait" />
     </div>
