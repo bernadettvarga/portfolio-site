b6f1221265f4f97f84f1e252820c8bbe95369d64
Bernadett Varga <detty.v@gmail.com>
2022-06-01 09:24:34 +0200

    feat: add page / fashion

---
+++ components/content/gallery/fashion.js
@@ -0,0 +1,21 @@
+export default {
+  folder: 'overview',
+  items: [
+    {
+      src: 'te-3',
+      type: 'portrait',
+    },
+    {
+      src: 'mad-prod-2',
+      type: 'portrait',
+    },
+    {
+      src: 'enroute-french-2-2',
+      type: 'portrait',
+    },
+    {
+      src: 'sb-1',
+      type: 'landscape',
+    },
+  ],
+}
