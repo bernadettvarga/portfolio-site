f43162a99ad8f17c1bf50e049ad0046498114bb7
Bernadett Varga <detty.v@gmail.com>
2022-06-02 10:10:51 +0200

    feat: add lazy loading to bg images

--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="overflow-y-scroll w-full p-4 md:p-6 md:pt-0">
     <div class="grid grid-cols-2 md:grid-cols-3 gap-4 md:gap-6">
-      <div class="project project--portrait te-3"></div>
-      <div class="project project--portrait mad-prod-2"></div>
+      <div class="project project--portrait" lazy-background="~assets/img/overview/te-3.jpg"></div>
+      <div class="project project--portrait" lazy-background="~assets/img/overview/mad-prod-2.jpg"></div>
       <div class="project--intro">
         <div class="md:absolute md:inset-0 flex items-end">
           <div class="pt-5 md:pt-0 md:pl-10 md:pb-10">
@@ -11,8 +11,8 @@
           </div>
         </div>
       </div>
-      <div class="project project--landscape sb-1"></div>
-      <div class="project project--portrait enroute-french-2-2"></div>
+      <div class="project project--landscape" lazy-background="~assets/img/overview/sb-1.jpg"></div>
+      <div class="project project--portrait"  lazy-background="~assets/img/overview/enroute-french-2-2.jpg"></div>
     </div>
   </div>
 </template>
@@ -22,18 +22,3 @@ export default {
   name: 'Projects',
 }
 </script>
-
-<style>
-.te-3 {
-  background-image: url('~assets/img/overview/te-3.jpg');
-}
-.sb-1 {
-  background-image: url('~assets/img/overview/sb-1.jpg');
-}
-.mad-prod-2 {
-  background-image: url('~assets/img/overview/mad-prod-2.jpg');
-}
-.enroute-french-2-2 {
-  background-image: url('~assets/img/overview/enroute-french-2-2.jpg');
-}
-</style>
