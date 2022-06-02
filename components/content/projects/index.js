6750b314db95edac7d1d801dac9ee0c38662934d
Bernadett Varga <detty.v@gmail.com>
2022-06-02 12:09:21 +0200

    feat: get content dynamically

---
+++ components/content/projects/index.js
@@ -0,0 +1,44 @@
+export default [
+  {
+    img: {
+      src: 'overview/te-3.jpg',
+      type: 'portrait',
+    },
+    title: 'Eszter Tóth',
+    subtitle: 'fashion portraits',
+    slug: 'toth-eszter',
+  },
+  {
+    img: {
+      src: 'overview/mad-prod-2.jpg',
+      type: 'portrait',
+    },
+    title: 'Marie Anne Design',
+    subtitle: 'product photography',
+    slug: 'marie-anne-design',
+  },
+  {
+    type: 'intro',
+    title: 'Projects',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.',
+  },
+  {
+    img: {
+      src: 'overview/sb-1.jpg',
+      type: 'landscape',
+    },
+    title: 'Betty Somody',
+    subtitle: 'themed portraits',
+    slug: 'somody-betty',
+  },
+  {
+    img: {
+      src: 'overview/enroute-french-2-2.jpg',
+      type: 'portrait',
+    },
+    title: 'En Route',
+    subtitle: 'clothing store campaign',
+    slug: 'en-route-french-style',
+  },
+]
--- pages/projects/index.vue
+++ pages/projects/index.vue
@@ -1,32 +1,49 @@
 <template>
   <div class="overflow-y-scroll w-full p-4 md:p-6 md:pt-0">
     <div class="grid grid-cols-2 md:grid-cols-3 gap-4 md:gap-6">
-      <div class="project project--portrait" lazy-background="~assets/img/overview/te-3.jpg">
-        <div class="project__overlay">
-          <div class="project__overlay__details">
-            <p class="font-display text-5xl">Eszter Tóth</p>
-            <div class="project__overlay__details__separator"></div>
-            <p class="uppercase tracking-widest">fashion portraits</p>
+      <div
+        v-for="(project, index) in projects"
+        :key="project.index"
+        :class="project.img ? `project project--${project.img.type}`: ''"
+        :lazy-background="project.img ? require(`~/assets/img/${project.img.src}`) : ''"
+      >
+        <template v-if="project.type === 'intro'">
+          <div class="hidden md:block project--intro">
+            <div class="md:absolute md:inset-0 flex items-end">
+              <div class="pt-5 md:pt-0 md:pl-10 md:pb-10">
+                <h1 class="text-3xl md:text-4xl">{{ project.title }}</h1>
+                <p class="w-56 font-display leading-5 text-lg pt-2">{{ project.description }}</p>
+              </div>
+            </div>
           </div>
-        </div>
+        </template>
+        <template v-else>
+          <NuxtLink :to="`/projects/${project.slug}`">
+            <div class="project__overlay">
+              <div class="project__overlay__details">
+                <p v-if="project.title" class="font-display text-5xl">{{ project.title }}</p>
+                <template v-if="project.subtitle">
+                  <div class="project__overlay__details__separator"></div>
+                  <p class="uppercase tracking-widest">{{ project.subtitle }}</p>
+                </template>
+              </div>
+            </div>
+          </NuxtLink>
+        </template>
       </div>
-      <div class="project project--portrait" lazy-background="~assets/img/overview/mad-prod-2.jpg"></div>
-      <div class="hidden md:block project--intro">
-        <div class="md:absolute md:inset-0 flex items-end">
-          <div class="pt-5 md:pt-0 md:pl-10 md:pb-10">
-            <h1 class="text-3xl md:text-4xl">Projects</h1>
-            <p class="w-56 font-display leading-5 text-lg pt-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-          </div>
-        </div>
-      </div>
-      <div class="project project--landscape" lazy-background="~assets/img/overview/sb-1.jpg"></div>
-      <div class="project project--portrait"  lazy-background="~assets/img/overview/enroute-french-2-2.jpg"></div>
     </div>
   </div>
 </template>
 
 <script>
+import contentArray from '@/components/content/projects/index.js'
+
 export default {
   name: 'Projects',
+  data() {
+    return {
+      projects: contentArray,
+    }
+  },
 }
 </script>
